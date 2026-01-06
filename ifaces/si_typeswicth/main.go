5f125a47d8a0fc2d1977d99d20b726f6457a7683
Yonko Shanks <kbaizhigit@gmail.com>
2026-01-07 02:00:16 +0500

    si_typeswitch fix

--- ifaces/si_typeswicth/main.go
+++ ifaces/si_typeswicth/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func test(x interface{}) {
-	switch x.(type) {
+	switch x := x.(type) {
 	case int:
 		fmt.Println("int", x)
 	case string:
@@ -13,12 +13,11 @@ func test(x interface{}) {
 	case nil:
 		fmt.Println("nil", x)
 	case func() int:
-		f := x.(func() int)
-		if f == nil {
+		if x == nil {
 			fmt.Println("func is nil")
 			return
 		}
-		fmt.Println("func", f())
+		fmt.Println("func", x())
 	default:
 		fmt.Println("unknown")
 	}
