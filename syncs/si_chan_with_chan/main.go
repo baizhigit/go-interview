bf59aac318147d233c2c27522649ef9ea9d28ae8
Yonko Shanks <kbaizhigit@gmail.com>
2026-01-09 20:18:22 +0500

    si-recur chan

---
+++ syncs/si_chan_with_chan/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+type c chan c
+
+func main() {
+	fmt.Println("main start")
+
+	var c = make(c, 1)
+	c <- c
+
+	for i := 0; i < 1000; i++ {
+		select {
+		case <-c:
+		case <-c:
+			c <- c
+		default:
+			fmt.Println(i)
+			return
+		}
+	}
+}
